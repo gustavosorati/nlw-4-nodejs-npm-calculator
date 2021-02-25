0325fd87f6390a00d054ffc35c2e4593edf93bd2
gustavo-sorati <gustavo.sorati@outlook.com>
2021-02-25 00:44:57 -0300

    day 3

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,12 @@
-import {Request, Response} from 'express';
-import { getRepository } from 'typeorm';
-import {User} from '../models/User';
-
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
     async create(req: Request, res: Response) {
         const {name, email} = req.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         // SELECT * FROM USERS WHERE EMAIL = "EMAIL"
         const userAlreadyExists = await usersRepository.findOne({
@@ -27,11 +26,8 @@ class UserController {
 
         await usersRepository.save(user);
 
-
-
-
-        return res.json(user);
+        return res.status(201).json(user);
     }
 }
 
-export {UserController}
+export { UserController };
